054b88feb5a7277ad11f98e74f36b34c9b199341
ranju999 <ranjuvishwakarma.50@gmail.com>
2025-01-18 08:06:32 +0530

    Update stream_routes.py

--- web/stream_routes.py
+++ web/stream_routes.py
@@ -6,12 +6,12 @@ import secrets
 import mimetypes
 from aiohttp import web
 from aiohttp.http_exceptions import BadStatusLine
-from biisal.bot import multi_clients, work_loads, StreamBot
-from biisal.server.exceptions import FIleNotFound, InvalidHash
-from biisal import StartTime, __version__
-from ..utils.time_format import get_readable_time
-from ..utils.custom_dl import ByteStreamer
-from biisal.utils.render_template import render_page
+from web.bot import multi_clients, work_loads, StreamBot
+from web.server.exceptions import FIleNotFound, InvalidHash
+from web import WebTime, __version__
+from web.utils.time_format import get_readable_time
+from web.utils.custom_dl import ByteStreamer
+from web.utils.render_template import render_page
 from info import *
 
 routes = web.RouteTableDef()
@@ -21,8 +21,8 @@ async def root_route_handler(_):
     return web.json_response(
         {
             "server_status": "running",
-            "uptime": get_readable_time(time.time() - StartTime),
-            "telegram_bot": "@" + StreamBot.username,
+            "uptime": get_readable_time(time.time() - WebTime),
+            "telegram_bot": "@" + webBot.username,
             "connected_bots": len(multi_clients),
             "loads": dict(
                 ("bot" + str(c + 1), l)
