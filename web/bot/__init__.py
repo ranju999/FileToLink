3d31b502c5fa92964340d67b66efb2b4bcb82b99
ranju999 <ranjuvishwakarma.50@gmail.com>
2025-01-18 07:55:59 +0530

    Create __init__.py

---
+++ web/bot/__init__.py
@@ -0,0 +1,15 @@
+from pyrogram import Client
+from info import *
+from os import getcwd
+
+webxav= Client(
+    name='Web Streamer',
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    sleep_threshold=SLEEP_THRESHOLD,
+    workers=WORKERS
+)
+
+multi_clients = {}
+work_loads = {}
