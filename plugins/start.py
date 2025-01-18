986fce04c73ad46b7c89b2d3ab77e65849e7a4b0
ranju999 <ranjuvishwakarma.50@gmail.com>
2025-01-18 09:31:36 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,26 +1,74 @@
-import random
+import os, random, asyncio
 from Script import script
-from pyrogram import Client, filters, enums
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply, CallbackQuery
-from info import LOG_CHANNEL
 from database.users_chats_db import db
-from utils import temp
+from pyrogram import Client, filters, enums
+from pyrogram.errors import *
+from pyrogram.types import *
+from info import *
 
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id, message.from_user.first_name)
-        await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
-    rm = InlineKeyboardMarkup(
-        [[
-            InlineKeyboardButton("✨ Update Channel", url="https://t.me/")
-        ]] 
-    )
-    await client.send_message(
-        chat_id=message.from_user.id,
-        text=script.START_TXT.format(message.from_user.mention),
-        reply_markup=rm,
-        parse_mode=enums.ParseMode.HTML
-    )
-    return
-
+        await client.send_message(LOG_CHANNEL, script.LOG_TEXT.format(message.from_user.id, message.from_user.mention))
+    if len(message.command) != 2:
+        buttons = [[
+            InlineKeyboardButton('🔍 ɢʀᴏᴜᴘ', url='https://t.me/'),
+            InlineKeyboardButton('🤖 ᴄʜᴀɴɴᴇʟ', url='https://t.me/')
+            ],[
+            InlineKeyboardButton('💁‍♀️ ʜᴇʟᴘ', callback_data='help'),
+            InlineKeyboardButton('😊 ᴀʙᴏᴜᴛ', callback_data='about')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        me = await client.get_me()
+        await message.reply_photo(
+            photo=(PICS),
+            caption=script.START_TXT.format(message.from_user.mention, me.mention),
+            reply_markup=reply_markup
+        )
+        return
+        
+@Client.on_callback_query()
+async def cb_handler(client: Client, query: CallbackQuery):
+    if query.data == "close_data":
+        await query.message.delete()
+    elif query.data == "about":
+        buttons = [[
+            InlineKeyboardButton('Hᴏᴍᴇ', callback_data='start'),
+            InlineKeyboardButton('🔒 Cʟᴏsᴇ', callback_data='close_data')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        me2 = (await client.get_me()).mention
+        await query.message.edit_text(
+            text=script.ABOUT_TXT.format(me2),
+            reply_markup=reply_markup,
+            parse_mode=enums.ParseMode.HTML
+        )
+    
+    elif query.data == "start":
+        buttons = [[
+            InlineKeyboardButton('🔍 ɢʀᴏᴜᴘ', url='https://t.me/'),
+            InlineKeyboardButton('🤖 ᴄʜᴀɴɴᴇʟ', url='https://t.me/')
+        ],[
+            InlineKeyboardButton('💁‍♀️ ʜᴇʟᴘ', callback_data='help'),
+            InlineKeyboardButton('😊 ᴀʙᴏᴜᴛ', callback_data='about')
+        ]]
+        me2 = (await client.get_me()).mention
+        await query.message.edit_text(
+            text=script.START_TXT.format(query.from_user.mention, me2),
+            reply_markup=reply_markup,
+            parse_mode=enums.ParseMode.HTML
+        )
+        
+    elif query.data == "help":
+        buttons = [[
+            InlineKeyboardButton('Hᴏᴍᴇ', callback_data='start'),
+            InlineKeyboardButton('🔒 Cʟᴏsᴇ', callback_data='close_data')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await query.message.edit_text(
+            text=script.HELP_TXT,
+            reply_markup=reply_markup,
+            parse_mode=enums.ParseMode.HTML
+        )  
+        
