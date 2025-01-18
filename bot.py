fb33c7f56e77b82df5d47dcbd391a0aebf6ec9b7
ranju999 <ranjuvishwakarma.50@gmail.com>
2025-01-18 10:17:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,45 +1,36 @@
 from web import WebTime, __version__
-from info import API_ID, API_HASH, BOT_TOKEN, PORT, ADMINS
-from aiohttp import web
 from web.server import web_server
-from pyrogram import Client, idle
-import pyromod.listen
-import os
-from typing import Union, Optional, AsyncGenerator
-from pyrogram import types
+from pyrogram import Client
+from info import API_ID, API_HASH, BOT_TOKEN, PORT
+from utils import tempa
+from aiohttp import web
 
 class Bot(Client):
+
     def __init__(self):
         super().__init__(
-            name="postforwarder",
+            name="FileToLinkBot",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            workers=200,
+            workers=10,
             plugins={"root": "plugins"},
-            sleep_threshold=15,
-            max_concurrent_transmissions=100,
+            sleep_threshold=5,
         )
 
     async def start(self):
-        await super().start()
-        me = await self.get_me()
-        self.mention = me.mention
-        self.username = me.username 
-        app = await web_server()
-        app['bot'] = self  # Pass bot instance to the app
-        runner = web.AppRunner(app)
-        await runner.setup()
-        bind_address = "0.0.0.0"       
-        await web.TCPSite(runner, bind_address, PORT).start()     
-        print(f"| ❤ |==> Iꜱ Sᴛᴀʀᴛᴇᴅ ɪɴɪᴛɪᴀᴛᴇᴅ {me.first_name} <==| 🍿 |")
-        await idle()
-        await self.send_message(ADMINS, f"**__{me.first_name}  Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️😅😅😅__**")
-        
+        await super().start() 
+        temp.BOT = self
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        await web.TCPSite(app, "0.0.0.0", PORT).start()
+        print(f"Bot started. Pyrogram v{__version__}")
+
     async def stop(self, *args):
         await super().stop()
         print("Bot stopped. Bye.")
-       
+    
+app = Bot()
+app.run()
 
-bot=Bot()
-bot.run()
+
